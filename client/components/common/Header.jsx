c3a57917c0d934631076981fc934075bb0777893
Hawra Ebrahim <hawrash18@gmail.com>
2025-08-25 08:04:43 +0300

    add foote,header and logo

---
+++ client/components/common/Header.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import "../styles/Header.scss";
+import EVLogo from "../../styles/EV-Black-Version.png"; // adjust path if needed
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-container">
+        {/* Logo */}
+        <Link to="/" className="header-logo">
+          <img src={EVLogo} alt="EV Logo" />
+        </Link>
+
+        {/* Profile */}
+        <nav className="header-nav">
+          <Link to="/profile" className="profile-link">Profile</Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
+ 
+
+
