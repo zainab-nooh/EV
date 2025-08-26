303b707c5eee31882898f89b537d3fff43849385
Ahmed <47med.aa3@gmail.com>
2025-08-26 15:53:49 +0300

    dunno

--- src/pages/BookingHistory/BookingHistory.jsx
+++ src/pages/BookingHistory/BookingHistory.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import styles from './bookinghistory.module.scss';
+import styles from './BookingHistory.module.scss';
 
 const BookingHistory = ({setUser}) => {
   const [bookings, setBookings] = useState([]);
