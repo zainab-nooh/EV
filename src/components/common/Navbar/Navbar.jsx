3dc65c2f51997f37e288da43367eb7af96906caa
zainab-nooh <zain3b.nooh@gmail.com>
2025-08-26 11:38:25 +0300

    fix issue

--- src/components/common/Navbar/Navbar.jsx
+++ src/components/common/Navbar/Navbar.jsx
@@ -1,27 +1,40 @@
 import { Link } from "react-router-dom";
-import "../../common/Navbar/Navbar.module.scss";
-import profile from "../../styles/6522516.png";
-import home from "../../styles/profile-removebg-preview.png";
-const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        <ul className="navbar-menu">
-          <li className="navbar-item">
-            <img src={home} alt="Home" className="navbar-home-image" />
-          </li>
-          <li className="navbar-item">
-            <Link to="/new-booking" className="navbar-link">New Booking</Link>
-          </li>
-          <li className="navbar-item">
-            <Link to="/logout" className="navbar-link">Logout</Link>
-          </li>
-        </ul>
-        <div className="navbar-profile">
-          <img src={profile} alt="Profile" className="navbar-profile-image" />
+import styles from"../../common/Navbar/Navbar.module.scss";
+import profile from '/Profile.png'
+import home from '/Home.png'
+ const Navbar = props => {
+    return (
+        <nav className={styles.navbar}>
+        <div className="navbar-container">
+            <ul className="navbar-menu">
+            <li className="navbar-item">
+            {/* <img src={home} alt="Home" className="navbar-home-image"/>  */}
+                <Link to="/home" className="navbar-link">Home</Link>
+             </li>
+            <li className="navbar-item">
+                <Link to="/bookings/new" className="navbar-link">New Booking</Link>
+            </li>
+            <li className="navbar-item">
+                <Link to="/profile" className="navbar-link">Profile</Link>
+            </li>
+            <li className="navbar-item">
+                <Link to="/bookings/history" className="navbar-link">Booking History</Link>
+            </li>
+            <li className="navbar-item">
+                <Link to="/auth" className="navbar-link">Logout</Link>
+            </li>
+            </ul>
+            {/* <div className="navbar-profile">
+            <img src={profile} alt="Profile" className="navbar-profile-image" />
+            </div> */}
         </div>
-      </div>
-    </nav>
-  );
-}
-export default Navbar;
+        </nav>
+    );
+ }
+    export default Navbar;
+
+
+
+
+
+
