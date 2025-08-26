e1dfd17d77f2b0d9a3a2c3cea288b98507efd3cf
Hawra Ebrahim <hawrash18@gmail.com>
2025-08-26 11:27:09 +0300

    edit common

--- src/components/common/Navbar/Navbar.jsx
+++ src/components/common/Navbar/Navbar.jsx
@@ -1,30 +1,29 @@
-
 import { Link } from "react-router-dom";
 import "../../common/Navbar/Navbar.module.scss"; 
 import profile from "../../styles/6522516.png"; 
 import home from "../../styles/profile-removebg-preview.png";
- const Navbar = props => {
-    return (
-        
-        <nav className="navbar">
-        <div className="navbar-container">
-            <ul className="navbar-menu">
-            className="navbar-item"
-            <image src={home} alt="Home" className="navbar-home-image" />
-            <li className="navbar-item">
-                <Link to="/new-booking" className="navbar-link">New Booking</Link>
-            </li>
-            <li className="navbar-item">
-                <Link to="/logout" className="navbar-link">Logout</Link>
-            </li>
-            </ul>
-            <div className="navbar-profile">
-            <img src={profile} alt="Profile" className="navbar-profile-image" />
-            </div>
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <ul className="navbar-menu">
+          <li className="navbar-item">
+            <img src={home} alt="Home" className="navbar-home-image" />
+          </li>
+          <li className="navbar-item">
+            <Link to="/new-booking" className="navbar-link">New Booking</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/logout" className="navbar-link">Logout</Link>
+          </li>
+        </ul>
+        <div className="navbar-profile">
+          <img src={profile} alt="Profile" className="navbar-profile-image" />
         </div>
-        </nav>
-    );
+      </div>
+    </nav>
+  );
+}
 
-
- }
-    export default Navbar;
+export default Navbar;
