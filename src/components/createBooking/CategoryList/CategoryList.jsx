965266a92dbb87cdd04c214182f0c155591ca5d3
Hawra Ebrahim <hawrash18@gmail.com>
2025-08-26 12:54:52 +0300

    done from scss category

---
+++ src/components/createBooking/CategoryList/CategoryList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import CategoryCard from "../CategoryCard/CategoryCard";
+import styles from "./CategoryList.scss";
+// import Category from "../../../../models/Category";
+
+const CategoryList = ({ categories}) => {
+  if (!categories || categories.length === 0) return <p>No Category found.</p>;
+
+  return (
+    <div className={styles.ItemDetail}>
+      {categories.map((category) => (
+        <CategoryCard
+          key={category._id}   
+          item={category}
+        
+        />
+      ))}
+    </div>
+  );
+};
+
+export default CategoryList;
