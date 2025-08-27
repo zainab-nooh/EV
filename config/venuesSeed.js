a08e2576936c0a808ca3a4f5c202115e9dd0fb93
Mohamed Ali Kadhem <mhejiy@gmail.com>
2025-08-27 16:03:36 +0300

    venuesSeed

---
+++ config/venuesSeed.js
@@ -0,0 +1,44 @@
+import venue1 from '../public/Venue1/venue1.png';
+import venue2 from '../public/Venue1/venue2.png';
+import venue3 from '../public/Venue1/venue3.png';
+import venue4 from '../public/Venue1/venue4.png';
+import venue5 from '../public/Venue1/venue5.png';
+
+
+      {
+        name: 'Haj Ahmed Mansour Al-Aali Hall',
+        picture: venue1,
+        deatails: 'The hall offers a luxurious and elegant design, accommodating over 500 guests.',
+        category: cateringCategory._id,
+        price: 500
+      },
+      
+      {
+        name: 'New Seasons Hall Ribbons',
+        picture: venue2,
+        deatails: 'Halls in Manama of this type typically handle 400-700 guests',
+        category: cateringCategory._id,
+        price: 3000
+      },
+            {
+        name: 'Style Events Hall',
+        picture: venue3,
+        deatails: 'Hall up to 60',
+        category: cateringCategory._id,
+        price: 25
+      },
+            {
+        name: 'Wedding Venue',
+        picture: venue4,
+        deatails: 'Hall up to 350',
+        category: cateringCategory._id,
+        price: 2000
+      },
+            {
+        name: 'Queen Rivers Bahrain hearts',
+        picture: venue5,
+        deatails: 'Hall up to 150',
+        category: cateringCategory._id,
+        price: 800
+      },
+    
