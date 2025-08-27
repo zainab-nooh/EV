61096cdb4c0f44a32b89393df1d06244dbd84c84
ayahalij <ayahali2003@gmail.com>
2025-08-27 09:41:58 +0300

    headr, navbar, footer changes

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,21 +1,24 @@
+
 import { Link } from "react-router-dom";
 import styles from "../../common/Header/Header.module.scss";
+import Navbar from "../Navbar/Navbar"; // Import Navbar component
 import EV from '/EV.png'
 
-const Header = () => {
+const Header = ({ setUser, ...props }) => { // Accept setUser prop to pass to Navbar
   return (
     <header className={styles.header}>
       <div className={styles['header-container']}>
         {/* Logo */}
-        <Link to="/home" className="header-logo">
+        <Link to="/home" className={styles['header-logo']}>
          <img src={EV} alt="EV" />
         </Link>
+        
+        {/* Navbar */}
+        <Navbar setUser={setUser} {...props} />
       </div>
     </header>
   );
 };
 
 export default Header;
- 
-
 
